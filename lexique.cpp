3a23d1129d90228a521d487a1d8e775bbb2c50e2
AndreLouisSt <stephantandrelouis@gmail.com>
2025-10-06 07:54:30 +0000

    Fin sauvegarde

--- lexique.cpp
+++ lexique.cpp
@@ -6,31 +6,37 @@
 #include <fstream>
 #include <cstring>
 
-Lexique::Lexique(string nom, string nom_fichier) {
+Lexique::Lexique(string nom, string nom_fichier)
+{
     string contenu;
-    if(!util::readFileIntoString(nom_fichier, contenu)) {
+    if (!util::readFileIntoString(nom_fichier, contenu))
+    {
         cerr << "Impossible de lire le fichier." << endl;
     }
 
     util::remove_punctuation(contenu);
 
-    //Découpage par mots
-    //Il faut dupliquer la string pour respecter le const de ".c_str()"
-    char* dup = strdup(contenu.c_str());
-    char* mot;
+    // Découpage par mots
+    // Il faut dupliquer la string pour respecter le const de ".c_str()"
+    char *dup = strdup(contenu.c_str());
+    char *mot;
     mot = strtok(dup, " \r\n");
-    while (mot != NULL) {
+
+    while (mot != NULL)
+    {
         string mot_string = string(mot);
-        //Si le mot est déjà dans le lexique
-        if(occurences.find(mot_string) != occurences.end()) {
-            occurences[mot_string] = occurences[mot_string]+1;
-        } else {
+        // Si le mot est déjà dans le lexique
+        if (occurences.find(mot_string) != occurences.end())
+        {
+            occurences[mot_string] = occurences[mot_string] + 1;
+        }
+        else
+        {
             occurences[mot_string] = 1;
         }
         cout << mot_string << " = " << occurences[mot_string] << endl;
         mot = strtok(NULL, " \r\n");
     }
-
     free(dup);
 }
 
@@ -43,16 +49,21 @@ void Lexique::setNom(string nom) { this->nom = nom; }
 void Lexique::setOccurences(map<string, uint16_t> occurences) { this->occurences = occurences; }
 
 // Fonctions
-void sauvegarderLexique(string texte) // Sauvegarde d'un lexique
+void Lexique::sauvegarderLexique(string texte) // Sauvegarde d'un lexique
 {
-    ofstream fichier(texte);                   //  Création du fichier
-    if (fichier)                               // si l'ouverture a réussi
+    ofstream fichier(texte); //  Création du fichier
+    if (fichier.is_open())   // Si ouverture OK
     {
-        // instructions
-        fichier.close(); // je referme le fichier
+        fichier << "Nom : " << this->nom << endl;
+        for (map<string, uint16_t>::iterator itmap = occurences.begin(); itmap != occurences.end(); ++itmap)
+        {
+            fichier << "dict[" << itmap->first << "] = " << itmap->second << endl;
+        }
+        fichier.close(); // Fermeture fichier
+        cout << "Ecriture dans " << texte << "terminée." << endl;
     }
     else
     {
         cerr << "Erreur à l'ouverture" << endl;
-    } // sinon
+    }
 }
