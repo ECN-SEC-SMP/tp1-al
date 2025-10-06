a6706adc0a7e11b4eb1f8477dc0dd7b5ef18e45c
AndreLouisSt <stephantandrelouis@gmail.com>
2025-10-06 10:12:43 +0000

    en curs

--- sublexique.hpp
+++ sublexique.hpp
@@ -5,11 +5,12 @@
 #include <map>
 #include <iostream>
 #include <cstdint>
+#include "lexique.hpp"
 
 using namespace std;
 
 // Création de la classe
-class SubLexique
+class SubLexique : public Lexique
 {
 
 private:
